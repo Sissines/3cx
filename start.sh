94a882b0bf8e4d3d123cbde5f1c518f523ada57d
Sissines <92338885+Sissines@users.noreply.github.com>
2022-02-22 15:36:26 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,37 +9,23 @@ sudo apt upgrade -y
 clear
 echo "$(tput setaf 2)     Update Complete"
 echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 3)     Full-Upgrade Started"
-sudo apt full-upgrade -y
 clear
 echo "$(tput setaf 2)     Update Complete"
 echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 2)     Full-Upgrade Complete"
-echo "$(tput setaf 3)     Distro Upgrading"
-sudo apt dist-upgrade -y
-clear
-echo "$(tput setaf 2)     Update Complete"
-echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 2)     Full-Upgrade Complete"
-echo "$(tput setaf 2)     Distro Upgraded"
 echo "$(tput setaf 3)     Cleaning File System"
 sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 clear
 echo "$(tput setaf 2)     Update Complete"
 echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 2)     Full-Upgrade Complete"
-echo "$(tput setaf 2)     Distro Upgraded"
 echo "$(tput setaf 2)     File System Cleaned"
 echo "$(tput setaf 3)     Creating 3cx Installer"
 touch 3cx.sh
-chmod +x 3cx.sh
+sudo chmod +x 3cx.sh
 echo '#!/bin/bash' >> ./3cx.sh
 echo 'wget https://downloads-global.3cx.com/downloads/misc/d10pi.zip; sudo bash d10pi.zip' >> ./3cx.sh
 clear
 echo "$(tput setaf 2)     Update Complete"
 echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 2)     Full-Upgrade Complete"
-echo "$(tput setaf 2)     Distro Upgraded"
 echo "$(tput setaf 2)     File System Cleaned"
 echo "$(tput setaf 2)     3cx Installer Created"
 echo "$(tput setaf 3)     Installing Xrdp"
@@ -47,8 +33,6 @@ sudo apt install xrdp -y
 clear
 echo "$(tput setaf 2)     Update Complete"
 echo "$(tput setaf 2)     Upgrade Complete"
-echo "$(tput setaf 2)     Full-Upgrade Complete"
-echo "$(tput setaf 2)     Distro Upgraded"
 echo "$(tput setaf 2)     File System Cleaned"
 echo "$(tput setaf 2)     3cx Installer Created"
 echo "$(tput setaf 2)     Installed Xrdp"
